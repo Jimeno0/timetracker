8b4849a66e2f30e280adc93c98b58bdfb45b6928
Jimeno0 <carlospj@icloud.com>
2016-11-14 12:22:32 +0100

    adding images to our project

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,9 +1,17 @@
 class SiteController < ApplicationController
   def home
-    
   end
 
   def contact
+  end
+
+  def calculator
+  end
+
+  def calculate
+    @first = params[:first_num].to_f
+    @second = params[:second_num].to_f
+    @result = @first + @second
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get '/', to: 'site#home'
 
   get '/contact', to: 'site#contact'
+
+  get '/calculator', to: 'site#calculator'
+
+  post '/calculate', to: 'site#calculate'
 end
 
