987ce3821cafa2cdcfd73099dd25d19c95247475
Jimeno0 <carlospj@icloud.com>
2016-11-17 11:33:15 +0100

    adding update stuff :hammer:

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -10,15 +10,28 @@ class TimeEntriesController < ApplicationController
   end
 
   def create
-    puts params.inspect
     @project = Project.find(params[:project_id])
-    @entry = @project.time_entries.new(
-      hours: params[:time_entry][:hours],
-      minutes: params[:time_entry][:minutes],
-      date: params[:time_entry][:date],
-    )
+    @entry = @project.time_entries.new(entry_params)
 
     render "new" unless @entry.save
     redirect_to '/projects/#{params[:project_id]}/time_entries'
+  end
+
+  def edit
+    @project = Project.find(params[:project_id])
+    @entry = @project.time_entries.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @entry = @project.time_entries.find(params[:id])
+
+    @entry.update(entry_params)
+    redirect_to '/projects/#{params[:project_id]}/time_entries'
+  end
+
+  private
+  def entry_params
+   params.require(:time_entry).permit(:hours, :minutes, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,12 @@ Rails.application.routes.draw do
 
   get '/projects/:project_id/time_entries', to: 'time_entries#index'
   get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+  get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
 
   post '/projects/:project_id/time_entries', to: 'time_entries#create',
    as: :project_time_entries
+
+  patch '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#update',
+    as: :project_time_entry
 end
 
